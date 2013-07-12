6e1f5b1e1b191873142f597df3a788676852809d
manisero <michalaniserowicz@gmail.com>
2013-07-12 23:13:59 +0200

    Added Token field to TrelloKeys

--- Trellendar/Trellendar.DataAccess/Trello/TrelloKeys.cs
+++ Trellendar/Trellendar.DataAccess/Trello/TrelloKeys.cs
@@ -4,5 +4,6 @@
     {
         public const string APPLICATION_KEY = "substitutewithyourapplicationkey";
         public const string SECRET_KEY = "substitutewithyoursecretkey";
+        public const string TOKEN = "substitutewithyourtoken";
     }
 }
