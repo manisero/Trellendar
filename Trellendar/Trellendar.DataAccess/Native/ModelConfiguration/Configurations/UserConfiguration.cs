da232c968187453a2e75dfa4bd3d30fc5beb4ded
manisero <michalaniserowicz@gmail.com>
2013-07-16 20:43:35 +0200

    Configured User entity

--- Trellendar/Trellendar.DataAccess/Native/ModelConfiguration/Configurations/UserConfiguration.cs
+++ Trellendar/Trellendar.DataAccess/Native/ModelConfiguration/Configurations/UserConfiguration.cs
@@ -7,7 +7,12 @@ namespace Trellendar.DataAccess.Native.ModelConfiguration.Configurations
     {
         protected override void ConfigureEntity(EntityTypeConfiguration<User> entity)
         {
-            
+            entity.Property(x => x.Email).IsRequired().HasMaxLength(256);
+            entity.Property(x => x.TrelloBoardID).IsRequired().HasMaxLength(100);
+            entity.Property(x => x.TrelloAccessToken).IsRequired().HasMaxLength(100);
+            entity.Property(x => x.CalendarID).IsRequired().HasMaxLength(100);
+            entity.Property(x => x.CalendarAccessToken).IsRequired().HasMaxLength(100);
+            entity.Property(x => x.CalendarRefreshToken).IsRequired().HasMaxLength(100);
         }
     }
 }
