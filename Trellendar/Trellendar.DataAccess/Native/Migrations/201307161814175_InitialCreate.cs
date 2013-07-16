62719efc003bd39c7f0a8611a440e15c5ac09efd
manisero <michalaniserowicz@gmail.com>
2013-07-16 20:16:21 +0200

    Generated initial migration

---
+++ Trellendar/Trellendar.DataAccess/Native/Migrations/201307161814175_InitialCreate.cs
@@ -0,0 +1,32 @@
+namespace Trellendar.DataAccess.Native.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserID = c.Int(nullable: false, identity: true),
+                        Email = c.String(),
+                        TrelloBoardID = c.String(),
+                        TrelloAccessToken = c.String(),
+                        CalendarID = c.String(),
+                        CalendarAccessToken = c.String(),
+                        CalendarAccessTokenExpirationTS = c.DateTime(nullable: false),
+                        CalendarRefreshToken = c.String(),
+                    })
+                .PrimaryKey(t => t.UserID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
