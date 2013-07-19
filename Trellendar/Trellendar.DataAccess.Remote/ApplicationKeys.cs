353039d84a661f8fcffdfa9a73131b5bd99ba1c6
manisero <michalaniserowicz@gmail.com>
2013-07-19 22:51:51 +0200

    Renamed DataAccess project to DataAccess.Remote

---
+++ Trellendar/Trellendar.DataAccess.Remote/ApplicationKeys.cs
@@ -0,0 +1,12 @@
+﻿namespace Trellendar.DataAccess
+{
+    internal static class ApplicationKeys
+    {
+        public const string TRELLO_APPLICATION_KEY = "confidential";
+        public const string TRELLO_APPLICATION_SECRET = "confidential";
+
+        public const string GOOGLE_API_CLIENT_ID = "confidential";
+        public const string GOOGLE_API_CLIENT_SECRET = "confidential";
+        public const string GOOGLE_API_REDIRECT_URI = "confidential";
+    }
+}
