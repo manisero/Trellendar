1522d6dd41ab08c180d86a0aa1191d8953d7f5b2
manisero <michalaniserowicz@gmail.com>
2013-07-21 21:07:13 +0200

    Created UserPreferences entity

---
+++ Trellendar/Trellendar.DataAccess.Local/ModelConfiguration/Configurations/UserPreferencesConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Trellendar.Domain.Trellendar;
+
+namespace Trellendar.DataAccess.Local.ModelConfiguration.Configurations
+{
+    public class UserPreferencesConfiguration : EntityConfigurationBase<UserPreferences>
+    {
+        protected override void ConfigureEntity(EntityTypeConfiguration<UserPreferences> entity)
+        {
+            entity.Property(x => x.CardEventNameTemplate).HasMaxLength(50);
+        }
+    }
+}
---
+++ Trellendar/Trellendar.Domain/Trellendar/UserPreferences.cs
@@ -0,0 +1,11 @@
+﻿namespace Trellendar.Domain.Trellendar
+{
+    public class UserPreferences
+    {
+        public int UserID { get; set; }
+
+        public User User { get; set; }
+
+        public string CardEventNameTemplate { get; set; }
+    }
+}
