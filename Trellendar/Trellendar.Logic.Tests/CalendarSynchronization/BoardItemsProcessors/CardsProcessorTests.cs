d95b379e475192a59a05f8582255fef7c2a1e6a1
manisero <michalaniserowicz@gmail.com>
2013-07-22 22:00:28 +0200

    Started testing CardsProcessor

--- Trellendar/Trellendar.Logic.Tests/CalendarSynchronization/BoardItemsProcessors/CardsProcessorTests.cs
+++ Trellendar/Trellendar.Logic.Tests/CalendarSynchronization/BoardItemsProcessors/CardsProcessorTests.cs
@@ -1,6 +1,27 @@
-﻿namespace Trellendar.Logic.Tests.CalendarSynchronization.BoardItemsProcessors
+﻿using AutoMoq;
+using FizzWare.NBuilder;
+using NUnit.Framework;
+using Trellendar.Domain.Calendar;
+using Trellendar.Domain.Trello;
+using Trellendar.Logic.CalendarSynchronization.BoardItemsProcessors;
+
+namespace Trellendar.Logic.Tests.CalendarSynchronization.BoardItemsProcessors
 {
+    [TestFixture]
     public class CardsProcessorTests
     {
+        [Test]
+        public void returns_null_for_closed_card()
+        {
+            var autoMoqer = new AutoMoqer();
+
+            // Arrange
+            var card = Builder<Card>.CreateNew().Build();
+
+            // Act
+            autoMoqer.Resolve<CardsProcessor>().Process(new[] { card }, "not important", new Event[0]);
+
+            // Assert
+        }
     }
 }
