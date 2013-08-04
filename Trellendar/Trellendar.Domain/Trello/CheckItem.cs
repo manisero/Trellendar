1d239584b4ab373f11ae0dae7c5e2bf18a21d409
manisero <michalaniserowicz@gmail.com>
2013-08-04 16:14:05 +0200

    Added CheckList and CheckItem entities to Trello domain

---
+++ Trellendar/Trellendar.Domain/Trello/CheckItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Trellendar.Domain.Trello
+{
+    public class CheckItem
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string State { get; set; }
+    }
+}
---
+++ Trellendar/Trellendar.Domain/Trello/CheckList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Trellendar.Domain.Trello
+{
+    public class CheckList
+    {
+        public string Id { get; set; }
+
+        public string IdCard { get; set; }
+
+        public string Name { get; set; }
+
+        public IList<CheckItem> CheckItems { get; set; }
+    }
+}
