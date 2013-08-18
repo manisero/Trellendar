1f1723087ad38b46b6c6364d6fa64609eede47e1
manisero <michalaniserowicz@gmail.com>
2013-08-18 12:19:16 +0200

    Created Trellendar.WebSite project

---
+++ Trellendar/Trellendar.WebSite/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿namespace Trellendar.WebSite
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
