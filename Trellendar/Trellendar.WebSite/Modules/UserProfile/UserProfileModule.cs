b1e0edb7a2ba5ff3cd7777e6b8d0ecf13784a2da
manisero <michalaniserowicz@gmail.com>
2013-08-19 01:18:09 +0200

    Started creating User Profile module

---
+++ Trellendar/Trellendar.WebSite/Modules/UserProfile/UserProfileModule.cs
@@ -0,0 +1,20 @@
+﻿using Nancy;
+using Nancy.Security;
+
+namespace Trellendar.WebSite.Modules.UserProfile
+{
+    public class UserProfileModule : NancyModule
+    {
+        public UserProfileModule() : base("UserProfile")
+        {
+            this.RequiresAuthentication();
+
+            Get["/"] = Index;
+        }
+
+        public dynamic Index(dynamic parameters)
+        {
+            return View["index"];
+        }
+    }
+}
