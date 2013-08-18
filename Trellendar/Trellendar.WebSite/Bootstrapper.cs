a03927a17b16caa9d3f797ce36be2cc76552c05e
manisero <michalaniserowicz@gmail.com>
2013-08-18 22:36:14 +0200

    Changed User.UserID property type from int to GUID

--- Trellendar/Trellendar.WebSite/Bootstrapper.cs
+++ Trellendar/Trellendar.WebSite/Bootstrapper.cs
@@ -1,11 +1,22 @@
-﻿using Nancy.Bootstrappers.Ninject;
+﻿using System.Data.Entity;
+using Nancy.Bootstrapper;
+using Nancy.Bootstrappers.Ninject;
 using Ninject;
+using Trellendar.DataAccess.Local;
+using Trellendar.DataAccess.Local.Migrations;
 using Trellendar.WebSite.Ninject;
 
 namespace Trellendar.WebSite
 {
     public class Bootstrapper : NinjectNancyBootstrapper
     {
+        protected override void ApplicationStartup(IKernel container, IPipelines pipelines)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<TrellendarDataContext, Configuration>());
+
+            base.ApplicationStartup(container, pipelines);
+        }
+
         protected override void ConfigureApplicationContainer(IKernel existingContainer)
         {
             new NinjectBootstrapper().RegisterApplicationModules(existingContainer);
