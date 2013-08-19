852159c44aa687cd4d9ba1c44bf0c56264b3856e
manisero <michalaniserowicz@gmail.com>
2013-08-19 02:49:39 +0200

    Implemented UserContext registration (TODO: REFACTOR)

--- Trellendar/Trellendar.WebSite/Modules/UserProfile/UserProfileModule.cs
+++ Trellendar/Trellendar.WebSite/Modules/UserProfile/UserProfileModule.cs
@@ -1,21 +1,32 @@
 ﻿using Nancy;
 using Nancy.Security;
 using Trellendar.Logic.UserManagement;
+using Trellendar.WebSite.Logic;
 using Trellendar.WebSite.Modules.UserProfile.Models;
 
 namespace Trellendar.WebSite.Modules.UserProfile
 {
     public class UserProfileModule : NancyModule
     {
+        private readonly IUserContextRegistrar _userContextRegistrar;
         private readonly IUserService _userService;
 
-        public UserProfileModule(IUserService userService)
+        public UserProfileModule(IUserContextRegistrar userContextRegistrar, IUserService userService)
             : base("UserProfile")
         {
+            _userContextRegistrar = userContextRegistrar;
             _userService = userService;
+
             this.RequiresAuthentication();
+            Before += RegisterUserContext;
 
             Get["/"] = Index;
+        }
+
+        private Response RegisterUserContext(NancyContext context)
+        {
+            _userContextRegistrar.Register(context.CurrentUser.UserName);
+            return null;
         }
 
         public dynamic Index(dynamic parameters)
---
+++ Trellendar/Trellendar.WebSite/Ninject/NinjectUserContextRegistrar.cs
@@ -0,0 +1,32 @@
+﻿using Ninject;
+using Trellendar.DataAccess.Local.Repository;
+using Trellendar.Domain.Trellendar;
+using Trellendar.Logic;
+using Trellendar.WebSite.Logic;
+
+namespace Trellendar.WebSite.Ninject
+{
+    public class NinjectUserContextRegistrar : IUserContextRegistrar
+    {
+        private readonly IKernel _kernel;
+
+        public NinjectUserContextRegistrar(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public void Register(string userEmail)
+        {
+            var user = _kernel.Get<IRepositoryFactory>().Create<User>().GetSingleOrDefault(x => x.Email == userEmail);
+
+            if (user != null)
+            {
+                _kernel.Bind<UserContext>().ToConstant(new UserContext { User = user });
+            }
+            else
+            {
+                _kernel.Unbind<UserContext>();
+            }
+        }
+    }
+}
