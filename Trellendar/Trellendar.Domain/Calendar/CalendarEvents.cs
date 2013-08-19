dfbffaec9c477909a1846a18dcdf7efaec8b52e6
manisero <michalaniserowicz@gmail.com>
2013-08-19 03:05:06 +0200

    Fixed CalendarEvents and CalendarList entities deserialization

--- Trellendar/Trellendar.Domain/Calendar/CalendarEvents.cs
+++ Trellendar/Trellendar.Domain/Calendar/CalendarEvents.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Trellendar.Domain.Calendar
 {
     public class CalendarEvents : Calendar
     {
+        [JsonProperty("items")]
         public IList<Event> Items { get; set; }
     }
 }
--- Trellendar/Trellendar.Domain/Calendar/CalendarList.cs
+++ Trellendar/Trellendar.Domain/Calendar/CalendarList.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Trellendar.Domain.Calendar
 {
     public class CalendarList
     {
+        [JsonProperty("items")]
         public IList<Calendar> Items { get; set; }
     }
 }
