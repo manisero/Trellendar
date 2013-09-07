0134d7fa29d7649bd9e77f027580f06ffcb032fe
manisero <michalaniserowicz@gmail.com>
2013-09-07 13:29:16 +0200

    Introduced 'utils' AngularJS module

--- Trellendar/Trellendar.WebSite/Nancy/Extensions/ViewExtensions.cs
+++ Trellendar/Trellendar.WebSite/Nancy/Extensions/ViewExtensions.cs
@@ -4,10 +4,11 @@ namespace Trellendar.WebSite.Nancy.Extensions
 {
     public static class ViewExtensions
     {
-        public static void Initialize(this NancyRazorViewBase view, string title = null, string ngController = null)
+        public static void Initialize(this NancyRazorViewBase view, string title = null, string ngModule = null, string ngController = null)
         {
             view.Layout = "Layout";
             view.ViewBag.Title = title;
+            view.ViewBag.NgModule = ngModule;
             view.ViewBag.NgController = ngController;
         }
     }
