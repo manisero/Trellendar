80ae3eb49da0cccd01036064be3318848f3de1a3
manisero <michalaniserowicz@gmail.com>
2013-09-07 11:14:37 +0200

    Introduced Initialize extension method for views

---
+++ Trellendar/Trellendar.WebSite/Nancy/Extensions/ViewExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Nancy.ViewEngines.Razor;
+
+namespace Trellendar.WebSite.Nancy.Extensions
+{
+    public static class ViewExtensions
+    {
+        public static void Initialize(this NancyRazorViewBase view, string title = null, string ngController = null)
+        {
+            view.Layout = "Layout";
+            view.ViewBag.Title = title;
+            view.ViewBag.NgController = ngController;
+        }
+    }
+}
