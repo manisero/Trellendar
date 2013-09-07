f11555e598e42d3dd3e4485a58b78162e1d5b703
manisero <michalaniserowicz@gmail.com>
2013-09-07 10:44:40 +0200

    TrelloLogIn: implemented AngularJS controller usage

---
+++ Trellendar/Trellendar.WebSite/Nancy/Conventions/StaticContentConventions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Nancy.Conventions;
+
+namespace Trellendar.WebSite.Nancy.Conventions
+{
+    public class StaticContentConventions : IConvention
+    {
+        public void Initialise(NancyConventions conventions)
+        {
+            conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("Scripts", "Scripts"));
+        }
+
+        public Tuple<bool, string> Validate(NancyConventions conventions)
+        {
+            return new Tuple<bool, string>(true, null);
+        }
+    }
+}
---
+++ Trellendar/Trellendar.WebSite/Scripts/_custom/utils.js
@@ -0,0 +1,4 @@
+﻿var redirect = function(url) {
+    window.open(url, '_blank');
+    return false;
+};
