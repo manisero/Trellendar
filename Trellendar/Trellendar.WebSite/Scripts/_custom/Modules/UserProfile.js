1657e4c6157b1f437cf0af5efae21f7fa5896e0c
manisero <michalaniserowicz@gmail.com>
2013-09-08 01:51:00 +0200

    Implemented selecting boards and calendars

--- Trellendar/Trellendar.WebSite/Scripts/_custom/Modules/UserProfile.js
+++ Trellendar/Trellendar.WebSite/Scripts/_custom/Modules/UserProfile.js
@@ -1,5 +1,44 @@
 ﻿angular.module('UserProfile', [])
     .controller("Controller", ['$scope', 'Model', function($scope, model) {
-        $scope.AvailableBoards = model.AvailableBoards;
-        $scope.AvailableCalendars = model.AvailableCalendars;
-    }]);
+        $scope.Bonds = model.BoardCalendarBonds;
+        $scope.SelectedBoard = null;
+        $scope.SelectedCalendar = null;
+    }])
+    .directive("board", function() {
+        return function (scope, iElement, iAttrs) {
+            var boardId = iAttrs.board;
+
+            iElement.bind('click', function () {
+                scope.SelectedBoard = boardId;
+                scope.SelectedCalendar = null;
+                scope.$apply();
+            });
+
+            scope.$watch("SelectedBoard", function (newValue) {
+                if (newValue == boardId) {
+                    iElement.addClass('selected');
+                } else {
+                    iElement.removeClass('selected');
+                }
+            });
+        };
+    })
+    .directive("calendar", function () {
+        return function (scope, iElement, iAttrs) {
+            var calendarId = iAttrs.calendar;
+
+            iElement.bind('click', function () {
+                scope.SelectedBoard = null;
+                scope.SelectedCalendar = calendarId;
+                scope.$apply();
+            });
+
+            scope.$watch("SelectedCalendar", function (newValue) {
+                if (newValue == calendarId) {
+                    iElement.addClass('selected');
+                } else {
+                    iElement.removeClass('selected');
+                }
+            });
+        };
+    });
