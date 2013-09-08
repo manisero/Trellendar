44088e9541efe631bf8a4c7762f5e843e78c149a
manisero <michalaniserowicz@gmail.com>
2013-09-08 10:16:32 +0200

    Refactored Layout.cshtml

--- Trellendar/Trellendar.WebSite/Nancy/Extensions/ViewExtensions.cs
+++ Trellendar/Trellendar.WebSite/Nancy/Extensions/ViewExtensions.cs
@@ -5,7 +5,7 @@ namespace Trellendar.WebSite.Nancy.Extensions
 {
     public static class ViewExtensions
     {
-        public static void Initialize(this NancyRazorViewBase view, string title = null, string ngModule = null, string ngController = null)
+        public static void Initialize<TModel>(this NancyRazorViewBase<TModel> view, string title = null, string ngModule = null, string ngController = null)
         {
             view.Layout = "Layout";
             view.ViewBag.Title = title;
