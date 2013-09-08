b5b44e48be4b86c99c41ac4210f3953b69191a24
manisero <michalaniserowicz@gmail.com>
2013-09-08 12:55:46 +0200

    Started refactoring UserProfile module

--- Trellendar/Trellendar.WebSite/Scripts/_custom/Modules/UserProfile.js
+++ Trellendar/Trellendar.WebSite/Scripts/_custom/Modules/UserProfile.js
@@ -3,16 +3,58 @@
         $scope.Bonds = model.BoardCalendarBonds;
         $scope.SelectedBoard = model.BoardCalendarBonds[0].BoardID;
         $scope.SelectedCalendar = null;
+        $scope.SelectBoard = function(boardId) {
+            $scope.SelectedBoard = boardId;
+            $scope.SelectedCalendar = null;
+        };
+        $scope.SelectCalendar = function(calendarId) {
+            $scope.SelectedBoard = null;
+            $scope.SelectedCalendar = calendarId;
+        };
+        $scope.BindBoard = function(boardId) {
+            for (var i = 0; i != $scope.Bonds.length; i++) {
+                if ($scope.Bonds[i].BoardID == boardId && $scope.Bonds[i].CalendarID == $scope.SelectedCalendar) {
+                    return;
+                }
+            }
+
+            $scope.Bonds.push({
+                BoardID: boardId,
+                CalendarID: $scope.SelectedCalendar
+            });
+        };
+        $scope.UnbindBoard = function(boardId) {
+            for (var i = 0; i != $scope.Bonds.length; i++) {
+                if ($scope.Bonds[i].BoardID == boardId && $scope.Bonds[i].CalendarID == $scope.SelectedCalendar) {
+                    $scope.Bonds.pop($scope.Bonds[i]);
+                    return;
+                }
+            }
+        };
+        $scope.BindCalendar = function(calendarId) {
+            for (var i = 0; i != $scope.Bonds.length; i++) {
+                if ($scope.Bonds[i].CalendarID == calendarId && $scope.Bonds[i].BoardID == $scope.SelectedBoard) {
+                    return;
+                }
+            }
+
+            $scope.Bonds.push({
+                BoardID: $scope.SelectedBoard,
+                CalendarID: calendarId
+            });
+        };
+        $scope.UnbindCalendar = function(calendarId) {
+            for (var i = 0; i != $scope.Bonds.length; i++) {
+                if ($scope.Bonds[i].CalendarID == calendarId && $scope.Bonds[i].BoardID == $scope.SelectedBoard) {
+                    $scope.Bonds.pop($scope.Bonds[i]);
+                    return;
+                }
+            }
+        };
     }])
     .directive("board", function() {
         return function(scope, iElement, iAttrs) {
             var boardId = iAttrs.board;
-
-            iElement.bind('click', function() {
-                scope.SelectedBoard = boardId;
-                scope.SelectedCalendar = null;
-                scope.$apply();
-            });
 
             scope.$watch("SelectedBoard", function(newValue) {
                 if (newValue == boardId) {
@@ -38,19 +80,6 @@
         return function(scope, iElement, iAttrs) {
             var boardId = iAttrs.bindBoard;
 
-            iElement.bind('click', function() {
-                for (var i = 0; i != scope.Bonds.length; i++) {
-                    if (scope.Bonds[i].BoardID == boardId && scope.Bonds[i].CalendarID == scope.SelectedCalendar) {
-                        return;
-                    }
-                }
-
-                scope.Bonds.push({
-                    BoardID: boardId,
-                    CalendarID: scope.SelectedCalendar
-                });
-            });
-
             scope.$watch("SelectedCalendar", function(newValue) {
                 if (scope.SelectedCalendar == null) {
                     iElement.addClass('hidden');
@@ -72,15 +101,6 @@
         return function(scope, iElement, iAttrs) {
             var boardId = iAttrs.unbindBoard;
 
-            iElement.bind('click', function() {
-                for (var i = 0; i != scope.Bonds.length; i++) {
-                    if (scope.Bonds[i].BoardID == boardId && scope.Bonds[i].CalendarID == scope.SelectedCalendar) {
-                        scope.Bonds.pop(scope.Bonds[i]);
-                        return;
-                    }
-                }
-            });
-
             scope.$watch("SelectedCalendar", function(newValue) {
                 for (var i = 0; i != scope.Bonds.length; i++) {
                     if (scope.Bonds[i].BoardID == boardId && scope.Bonds[i].CalendarID == newValue) {
@@ -96,12 +116,6 @@
     .directive("calendar", function() {
         return function(scope, iElement, iAttrs) {
             var calendarId = iAttrs.calendar;
-
-            iElement.bind('click', function() {
-                scope.SelectedBoard = null;
-                scope.SelectedCalendar = calendarId;
-                scope.$apply();
-            });
 
             scope.$watch("SelectedCalendar", function(newValue) {
                 if (newValue == calendarId) {
@@ -127,19 +141,6 @@
         return function(scope, iElement, iAttrs) {
             var calendarId = iAttrs.bindCalendar;
 
-            iElement.bind('click', function() {
-                for (var i = 0; i != scope.Bonds.length; i++) {
-                    if (scope.Bonds[i].CalendarID == calendarId && scope.Bonds[i].BoardID == scope.SelectedBoard) {
-                        return;
-                    }
-                }
-
-                scope.Bonds.push({
-                    BoardID: scope.SelectedBoard,
-                    CalendarID: calendarId
-                });
-            });
-
             scope.$watch("SelectedBoard", function(newValue) {
                 if (scope.SelectedBoard == null) {
                     iElement.addClass('hidden');
@@ -160,15 +161,6 @@
     .directive("unbindCalendar", function() {
         return function(scope, iElement, iAttrs) {
             var calendarId = iAttrs.unbindCalendar;
-
-            iElement.bind('click', function() {
-                for (var i = 0; i != scope.Bonds.length; i++) {
-                    if (scope.Bonds[i].CalendarID == calendarId && scope.Bonds[i].BoardID == scope.SelectedBoard) {
-                        scope.Bonds.pop(scope.Bonds[i]);
-                        return;
-                    }
-                }
-            });
 
             scope.$watch("SelectedBoard", function(newValue) {
                 for (var i = 0; i != scope.Bonds.length; i++) {
