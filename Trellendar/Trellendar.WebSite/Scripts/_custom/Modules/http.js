89e410fea764dbd969aaddaf5a5200f0fbdb4c95
manisero <michalaniserowicz@gmail.com>
2013-09-08 22:11:03 +0200

    Improved AjaxService registration

--- Trellendar/Trellendar.WebSite/Scripts/_custom/Modules/http.js
+++ Trellendar/Trellendar.WebSite/Scripts/_custom/Modules/http.js
@@ -1,22 +1,20 @@
 ﻿angular.module('http', [])
-    .factory('AjaxService', function() {
-        return {
-            send: function(request, successCallback, failureCallback) {
-                request
-                    .success(function(data) {
-                        if (data.Success) {
-                            if (successCallback != null) {
-                                successCallback(data.Data);
-                            }
-                        } else {
-                            if (failureCallback != null) {
-                                failureCallback(data.ErrorMessage);
-                            }
+    .service('AjaxService', function() {
+        this.send = function(request, successCallback, failureCallback) {
+            request
+                .success(function(data) {
+                    if (data.Success) {
+                        if (successCallback != null) {
+                            successCallback(data.Data);
                         }
-                    })
-                    .error(function() {
-                        $scope.Message = 'Unknown error';
-                    });
-            }
+                    } else {
+                        if (failureCallback != null) {
+                            failureCallback(data.ErrorMessage);
+                        }
+                    }
+                })
+                .error(function() {
+                    $scope.Message = 'Unknown error';
+                });
         };
     });
