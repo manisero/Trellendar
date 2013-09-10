6a92562ffd3d15ab3956bc3992bf14597715fd7b
manisero <michalaniserowicz@gmail.com>
2013-09-10 23:16:07 +0200

    Introduced Base module, organized custom scripts structure

--- Trellendar/Trellendar.WebSite/Scripts/_custom/Modules/UserProfile.js
+++ Trellendar/Trellendar.WebSite/Scripts/_custom/Modules/UserProfile.js
@@ -1,4 +1,4 @@
-﻿angular.module('UserProfile', ['localization', 'http'])
+﻿angular.module('UserProfile', ['Base', 'http'])
     .controller("Controller", ['$scope', 'Model', '$http', 'AjaxService', function($scope, model, $http, ajaxService) {
         $scope.Bonds = model.BoardCalendarBonds;
         $scope.SelectedBoard = model.BoardCalendarBonds.length != 0 ? model.BoardCalendarBonds[0].BoardID : null;
