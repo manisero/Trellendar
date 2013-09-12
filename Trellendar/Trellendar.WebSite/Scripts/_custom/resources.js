f019d91bcd8a0184b590434cb0791b94b2ca516b
maniserowicz <m.aniserowicz@funandmobile.com>
2013-09-12 16:30:14 +0200

    Created t4 template for javascript resources

--- Trellendar/Trellendar.WebSite/Scripts/_custom/resources.js
+++ Trellendar/Trellendar.WebSite/Scripts/_custom/resources.js
@@ -1,4 +1,20 @@
-﻿var resources = {
-    UnknownErrorMessage: 'Unknown error',
-    ProfileSavedMessage: 'Profile saved successfully'
-}
+﻿
+
+var resources = {
+	    BindAction: 'Bind',
+	    BoardsItem: 'Boards',
+	    CalendarsItem: 'Calendars',
+	    ClickAction: 'Click',
+	    GoogleLogInAction: 'Log in with Google',
+	    LogOutAction: 'Log out',
+	    ProfileSavedMessage: 'Your profile has been saved successfully',
+	    SaveAction: 'Save',
+	    TrelloAuthorizationPrompt: 'Click here to retrieve a token allowing Trellendar to read your Trello data:',
+	    TrelloLogInPageTitle: 'Trello Log In',
+	    TrelloTokenPrompt: 'Please paste the token here and click "submit" button:',
+	    UnbindAction: 'Unbind',
+	    UnknownErrorMessage: 'Unknown error occured',
+	    UserGreeting: 'Hello, {0}',
+	    UserProfilePageTitle: 'Your Profile',
+	    ViewProfileAction: 'View your profile',
+	}
